46e416ec2557759903f68dfb9692ae7f4a1a463b
Brett <brbiamafk@gmail.com>
2022-11-14 16:50:20 -0500

    Start setup of user DB

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,22 @@
 import os
-from flask import Flask, render_template
+
+from flask import Flask, render_template, url_for
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
 
 static_folder = os.path.abspath('../frontend/static')
 template_folder = os.path.abspath('../frontend/templates')
 app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+with app.app_context():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SECRET_KEY'] = '%C*F-JaNdRgUkXn2r5u8x/A?D(G+KbPe'
+    db = SQLAlchemy(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
 
 @app.route('/')
 def index():
@@ -15,7 +28,7 @@ def login():
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('register.html')
 
 @app.route('/collaborate')
 def collaborate():
